099250b68de05700e09fef18802698e2b58edb68
biggaji <tobiajibade2017@yandex.com>
2023-07-25 15:33:34 +0100

    updated database and graphql typedefs to match data structure

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,9 +1,12 @@
+import { dateScalarType } from "./typeDefs";
+
 const resolvers = {
   Query: {
     user: () => {
       return "Hey 0xAds";
     },
-  }
+  },
+  DateTime: dateScalarType,
 }
 
 export default resolvers;
